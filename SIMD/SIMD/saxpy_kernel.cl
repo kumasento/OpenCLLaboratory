fe9f6667c401ede5c6205c1548bdfaedc5532d37
Vincent Zhao <vincentzhaorz@gmail.com>
2014-12-15 11:51:56 +0800

    add SIMD experiments

---
+++ SIMD/SIMD/saxpy_kernel.cl
@@ -0,0 +1,89 @@
+__kernel 
+void saxpy_naive(__global float *X, 
+                 __global float *Y, 
+                 __global float *Z, 
+                 float alpha, 
+                 int dim)
+{
+    size_t gid = get_global_id(0) * dim;
+    size_t upper = gid + dim;
+    for (; gid < upper; gid++)
+        Z[gid] = X[gid] * alpha + Y[gid]; 
+}
+
+__kernel 
+void saxpy_dim4(__global float *X, 
+                __global float *Y, 
+                __global float *Z, 
+                float alpha) 
+{
+    size_t gid = get_global_id(0) * 4;
+    float4 x = (float4)(X[gid], X[gid+1], X[gid+2], X[gid+3]);
+    float4 y = (float4)(Y[gid], Y[gid+1], Y[gid+2], Y[gid+3]);
+    float4 z = x * alpha + y;
+    
+    //printf("%.3f %.3f %.3f %.3f\n", x.x, x.y, x.z, x.w);
+    //printf("%.3f %.3f %.3f %.3f\n", y.x, y.y, y.z, y.w);
+    //printf("%.3f %.3f %.3f %.3f\n", z.x, z.y, z.z, z.w);
+
+    Z[gid] = z.x;
+    Z[gid+1] = z.y;
+    Z[gid+2] = z.z;
+    Z[gid+3] = z.w;
+}
+
+__kernel 
+void saxpy_dim8(__global float *X, 
+                __global float *Y, 
+                __global float *Z, 
+                float alpha) 
+{
+    size_t gid = get_global_id(0) * 8;
+    float8 x = (float8)(X[gid], X[gid+1], X[gid+2], X[gid+3], X[gid+4], X[gid+5], X[gid+6], X[gid+7]);
+    float8 y = (float8)(Y[gid], Y[gid+1], Y[gid+2], Y[gid+3], Y[gid+4], Y[gid+5], Y[gid+6], Y[gid+7]);
+    float8 z = x * alpha + y;
+    
+    //printf("%.3f %.3f %.3f %.3f\n", x.x, x.y, x.z, x.w);
+    //printf("%.3f %.3f %.3f %.3f\n", y.x, y.y, y.z, y.w);
+    //printf("%.3f %.3f %.3f %.3f\n", z.x, z.y, z.z, z.w);
+
+    Z[gid]      = z.s0;
+    Z[gid+1]    = z.s1;
+    Z[gid+2]    = z.s2;
+    Z[gid+3]    = z.s3;
+    Z[gid+4]    = z.s4;
+    Z[gid+5]    = z.s5;
+    Z[gid+6]    = z.s6;
+    Z[gid+7]    = z.s7;
+}
+
+__kernel 
+void saxpy_dim16(__global float *X, 
+                __global float *Y, 
+                __global float *Z, 
+                float alpha) 
+{
+    size_t gid = get_global_id(0) * 16;
+    float16 x = (float16)(X[gid], X[gid+1], X[gid+2], X[gid+3], X[gid+4], X[gid+5], X[gid+6], X[gid+7], \
+                                X[gid+8], X[gid+9], X[gid+10], X[gid+11], X[gid+12], X[gid+13], X[gid+14], X[gid+15]);
+    float16 y = (float16)(Y[gid], Y[gid+1], Y[gid+2], Y[gid+3], Y[gid+4], Y[gid+5], Y[gid+6], Y[gid+7], \
+                                Y[gid+8], Y[gid+9], Y[gid+10], Y[gid+11], Y[gid+12], Y[gid+13], Y[gid+14], Y[gid+15]);
+    float16 z = x * alpha + y;
+
+    Z[gid]      = z.s0;
+    Z[gid+1]    = z.s1;
+    Z[gid+2]    = z.s2;
+    Z[gid+3]    = z.s3;
+    Z[gid+4]    = z.s4;
+    Z[gid+5]    = z.s5;
+    Z[gid+6]    = z.s6;
+    Z[gid+7]    = z.s7;
+    Z[gid+8]    = z.s8;
+    Z[gid+9]    = z.s9;
+    Z[gid+10]   = z.sa;
+    Z[gid+11]   = z.sb;
+    Z[gid+12]   = z.sc;
+    Z[gid+13]   = z.sd;
+    Z[gid+14]   = z.se;
+    Z[gid+15]   = z.sf;
+}
---
+++ SIMD/SIMD/src/saxpy_dim16.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <assert.h>
+
+//#include <CL/cl.h>
+
+#include "oclcommon.h"
+
+#define GROUP_SIZE 128
+
+int main(int argc, char *argv[]){
+    // check commandline parameters
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s [kernel] [length of vector]\n",
+                argv[0]);
+        exit(1);
+    }
+    
+    cl_int errorCode;
+    cl_device_type      deviceType = CL_DEVICE_TYPE_ALL;
+    cl_device_id *      devices = NULL;
+    cl_context          context = NULL;
+    cl_command_queue    cmdQueue = NULL;
+    cl_program          program = NULL;
+
+    char *kernelfile = argv[1];
+    int length = atoi(argv[2]);
+
+    assert(initialization(
+                deviceType,
+                devices,
+                &context,
+                &cmdQueue,
+                &program,
+                kernelfile));
+
+    float *X = (float*) malloc(sizeof(float)*length);
+    float *Y = (float*) malloc(sizeof(float)*length);
+    float *Z = (float*) malloc(sizeof(float)*length);
+
+    for (int i = 0; i < length; i++) {
+        X[i] = (float)i + 0.1;
+        Y[i] = (float)i + 0.2;
+        Z[i] = 0.0;
+    } 
+
+    cl_mem X_mem, Y_mem, Z_mem;
+    ALLOCATE_GPU_READ(X_mem, X, sizeof(float)*length);
+    ALLOCATE_GPU_READ(Y_mem, Y, sizeof(float)*length);
+    ALLOCATE_GPU_READ_WRITE_INIT(Z_mem, Z, sizeof(float)*length); 
+    
+    size_t globalSize[1] = {length/16};
+    size_t localSize[1] = {1};
+
+    float alpha = 0.2;
+    cl_kernel kernel = clCreateKernel(program, "saxpy_dim16", &errorCode); CHECKERROR;
+    errorCode = clSetKernelArg(kernel, 0, sizeof(cl_mem), &X_mem); CHECKERROR;
+    errorCode = clSetKernelArg(kernel, 1, sizeof(cl_mem), &Y_mem); CHECKERROR;
+    errorCode = clSetKernelArg(kernel, 2, sizeof(cl_mem), &Z_mem); CHECKERROR;
+    errorCode = clSetKernelArg(kernel, 3, sizeof(cl_float), &alpha); CHECKERROR;
+
+    errorCode = clEnqueueNDRangeKernel(cmdQueue, kernel, 1, NULL, globalSize, localSize, 0, NULL, NULL); CHECKERROR;
+    printf("Start to Run ...\n");
+    cl_event runEvent;
+    errorCode = clEnqueueNDRangeKernel(cmdQueue, kernel, 1, NULL, globalSize, localSize, 0, NULL, &runEvent); CHECKERROR;
+    errorCode = clFinish(cmdQueue);
+
+    printf("Execution Time: %.2fns\n", executionTime(runEvent) / length * 1e9);
+
+    printf("Start to Readback ...\n");
+    errorCode = clEnqueueReadBuffer(cmdQueue, Z_mem, CL_TRUE, 0, sizeof(float)*length, Z, 0, NULL, NULL); CHECKERROR;
+    
+    printf("Checking Correctness ...\n");
+    
+    for (int i = 0; i < length; i++) {
+        float res = X[i] * alpha + Y[i];
+        float ans = Z[i];
+        if (res - ans > 1E-4 || res - ans < -1E-4) {
+            printf("%.10f %.10f %.10f\n", res, ans, res-ans);
+            fprintf(stderr, "ERROR!");
+            exit(1);
+        }
+    }   
+    printf("OK\n");
+
+    return 0;
+}
